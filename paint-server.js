de9e900ad1e985eb81cbe420f895668f5afa373a
Chris <chris@chris-linux.(none)>
2013-02-27 11:30:43 -0700

    Server now persists all points (color and x/y) When a user connects it passes them all of this info

--- paint-server.js
+++ paint-server.js
@@ -1,16 +1,30 @@
 var io = require('socket.io').listen(1337, "0.0.0.0");
 
+//keep track of all the drawn points...
+var points = [];
+
 var activeClients = 0;
 io.sockets.on('connection', function(client){
   activeClients +=1;
   io.sockets.emit('new player', {clients:activeClients})
   client.on('disconnect', function(){clientDisconnect(client)});
-  client.on('pushUpdateToServer', function(data){clientUpdate(data)});
+  client.emit('pushUpdateToClient', {points:points}); 
+
+  //when a client sends us updates save them and push the info to all other clients
+  client.on('pushUpdateToServer', function(data){
+	for (var b in data.buffer) {
+		points.push({color:data.color, x:data.buffer[b].x, y:data.buffer[b].y});
+	}
+	clientUpdate(data)
+  });
 });
 
+function clear() {
+  points = [];
+}
 
 function clientUpdate(data) {
-  io.sockets.emit('pushUpdateToClients', {buffer:data.buffer});
+  io.sockets.emit('pushUpdateToClients', {buffer:data.buffer, color: data.color});
 };
 
 function clientDisconnect(client){
